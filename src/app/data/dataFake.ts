3f6124f6a6a25dae9db3fb9791b8be3786b7e214
Wallace Benites <148720798+wallaceeBenites@users.noreply.github.com>
2024-01-20 21:21:51 -0300

    teste

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":'1',
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder-1024x512.png"
+  },
+  {
+    "id":'2',
+    "title": "saiu novo filme",
+    "description": " bla bla bla ",
+    "photoCover":"https://milkmellow.com.br/wp-content/uploads/2023/06/O-Homem-de-Ferro.jpg"
+  }
+]
